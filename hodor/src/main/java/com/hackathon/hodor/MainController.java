1905fd98dbee5fd5127334596b5cb7dc00e09e25
Anuj Jain <anuj.jain@swiggy.in>
2017-07-15 19:32:42 +0530

    missing item implementation

--- hodor/src/main/java/com/hackathon/hodor/MainController.java
+++ hodor/src/main/java/com/hackathon/hodor/MainController.java
@@ -1,6 +1,7 @@
 package com.hackathon.hodor;
 
 import com.mysql.jdbc.Driver;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.web.bind.annotation.*;
@@ -19,7 +20,7 @@ public class MainController {
 	
 	public MainController() {
 		try {
-			sqlConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/swiggy?user=root&password=");
+			sqlConnection = DriverManager.getConnection("jdbc:mysql://localhost:3306/swiggy_hackathon?user=root&password=");
 		} catch (Exception e) {
 			// TODO: handle exception
 		}
@@ -45,12 +46,67 @@ public class MainController {
 
     @RequestMapping(value="/missing_item", method=RequestMethod.GET)
     public @ResponseBody Object getMissing(@RequestParam("name") String name) {
-        
-        MissingItem missingItem = new MissingItem();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        int area=0;
+        String cuisine="";
+        int restId=0;
+        try{
+            ps = sqlConnection.prepareStatement("select * from restaurants where name=\"" + name + "\"");
+            rs = ps.executeQuery();
+            while(rs.next()){
+                area = rs.getInt("area_id");
+                cuisine = rs.getString("cuisine");
+                restId=rs.getInt("id");
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        if(area==0)
+            return null;
         List<Item> items = new ArrayList<>();
-        Item item = new Item(130,"Kshatriya Kebab");
-        items.add(item);
-        missingItem.setItems(items);
+        try{
+            String sqlQuery = "select sum(quantity),item_price,m.`item_name`,r.`cuisine`,r.id from order_items o,restaurant_menu m,restaurants r " +
+                    "where o.`item_id`=m.`id` and m.`restaurant_id`=r.`id` and r.id!="+ restId +" and r.`area_id`="+area+" and r.`cuisine` like \"" +cuisine +
+                    "\" group by item_id order by sum(quantity) desc limit 5;";
+            ps = sqlConnection.prepareStatement(sqlQuery);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                Item item = new Item(Math.round(rs.getFloat("item_price")),rs.getString("item_name"));
+                items.add(item);
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+        List<String> existingItems = new ArrayList<>();
+        try{
+            ps = sqlConnection.prepareStatement("select * from restaurant_menu where restaurant_id=" + restId);
+            rs = ps.executeQuery();
+            while(rs.next()){
+                existingItems.add(rs.getString("item_name"));
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
+
+        MissingItem missingItem = new MissingItem();
+        List<Item> missingItems = new ArrayList<>();
+        for(Item item:items) {
+            boolean found=false;
+            for(String itemName :existingItems) {
+                if(itemName.equals(item.getName())){
+                    found=true;
+                }
+            }
+            if(!found) {
+                missingItems.add(item);
+            }
+        }
+
+
+        missingItem.setItems(missingItems);
         return missingItem;
     }
 
