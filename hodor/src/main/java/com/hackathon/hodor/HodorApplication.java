62ad6460c799604a245a83b0fe4dea5c22ddd2c3
Anuj Jain <anuj.jain@swiggy.in>
2017-07-15 12:56:19 +0530

    spring boot running

---
+++ hodor/src/main/java/com/hackathon/hodor/HodorApplication.java
@@ -0,0 +1,12 @@
+package com.hackathon.hodor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HodorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HodorApplication.class, args);
+	}
+}
