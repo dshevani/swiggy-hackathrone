87f68849cd3112fdfb2086d741721b2a84f91e35
Anuj Jain <anuj.jain@swiggy.in>
2017-07-15 17:04:15 +0530

    sql changes

--- hodor/src/main/java/com/hackathon/hodor/MainController.java
+++ hodor/src/main/java/com/hackathon/hodor/MainController.java
@@ -1,9 +1,12 @@
 package com.hackathon.hodor;
 
-import org.springframework.boot.*;
+import com.mysql.jdbc.Driver;
 import org.springframework.boot.autoconfigure.*;
-import org.springframework.stereotype.*;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
 import org.springframework.web.bind.annotation.*;
+
+import javax.sql.DataSource;
+import java.sql.*;
 
 
 @RestController
@@ -12,6 +15,21 @@ public class MainController {
 
     @RequestMapping("/")
     String home() {
+        Connection con = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try{
+             con = DriverManager.getConnection("jdbc:mysql://localhost:3306/swiggy_hackathon?user=root&password=");
+            ps = con.prepareStatement("select * from area");
+            rs = ps.executeQuery();
+            while(rs.next()){
+                System.out.println(rs.getString("name"));
+            }
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+
         return "Hello World!";
     }
 
