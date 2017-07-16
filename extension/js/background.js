3160cd6d9a51a56f52106280faede5c2930afe9e
Vikas S Yaligar <vikas.yaligar@flipkart.com>
2017-07-16 14:53:39 +0530

    integrated with popular

--- extension/js/background.js
+++ extension/js/background.js
@@ -38,5 +38,15 @@ chrome.runtime.onMessage.addListener(function(message, sendResponse) {
 			  chrome.tabs.sendMessage(tab.id, {message: data, type: message.type});
 			 });
 		});
+    } else if (message && message.type == 'get_popularity') {
+    	$.ajax({
+    		url: "http://172.16.120.190:8080/get_popular?name=" + message.rest_name,
+    	}).done(function(data) {
+			chrome.tabs.getSelected(null, function(tab) {
+			  console.log("Background script is sending a message to contentscript:'" + data +"'");
+			  console.log(data);
+			  chrome.tabs.sendMessage(tab.id, {message: data, type: message.type});
+			 });
+		});
     }
 });
